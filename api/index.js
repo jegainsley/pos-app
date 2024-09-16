199ee8ec70a60690e45a932aea8f635d317c8ad2
Justin <justin.gainsley@coinbase.com>
2024-09-16 12:47:48 -0400

    Fixed static file paths and server configuration

--- api/index.js
+++ api/index.js
@@ -12,12 +12,12 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+//app.get('/', (req, res) => {
+  //res.sendFile(path.join(__dirname, 'public', 'index.html'));
+//});
 
 // Endpoint to create a charge and generate a QR code
 app.post('/create-charge', async (req, res) => {
